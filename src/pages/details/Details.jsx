5dc9f85b72ad28c76534d922c0802563e245a4df
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-24 02:57:20 +0300

    Routes added

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,8 @@
+import './Details.css';
+
+function Details() {
+    return (
+        <div>Details</div>
+    )
+}
+export default Details
---
+++ src/pages/search/Search.jsx
@@ -0,0 +1,8 @@
+import './Search.css';
+
+function Search() {
+    return (
+        <div>Search</div>
+    )
+}
+export default Search
