825465f44a59cbc7692b607bc9dab916b8ad2e68
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-24 03:22:57 +0300

    Layout Created

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import Home from './pages/home/Home'
 import Search from './pages/search/Search'
 import Details from './pages/details/Details'
 import Create from './pages/create/Create'
+import MainLayout from './layouts/MainLayout'
 
 const routes = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/search', element: <Search /> },
-  { path: '/details/:id', element: <Details /> },
-  { path: '/create', element: <Create /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/search', element: <Search /> },
+      { path: '/details/:id', element: <Details /> },
+      { path: '/create', element: <Create /> },
+    ]
+  },
 ]);
 
 function App() {
