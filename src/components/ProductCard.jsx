2bb46ed252c1b46ae165318ce18b055865cb8456
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-24 05:12:16 +0300

    ProductCard
    Minor layout changes have been made to the Product Card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function ProductCard({ recipe }) {
     return (
-        <div className='col-3 mb-3'>
-            <div className="card" key={recipe.id}>
-                <img src={`/img/${recipe.image}`} alt={recipe.title} />
+        <div className='col-3 mb-3' key={recipe.id}>
+            <div className="card">
+                <img src={`/img/${recipe.image}`} alt={recipe.title} className='img-fluid' />
                 <div className='card-body'>
                     <h5 className='card-title'>{recipe.title}</h5>
                     <p className='card-text'>{recipe.explanation}</p>
