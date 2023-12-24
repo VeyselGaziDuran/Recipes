5956fa7cbb51487dac771cc266c3b8770ccb586b
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-24 20:27:39 +0300

    Form
    A form has been developed to add recipe ingredients.

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './Create.css';
 
@@ -8,16 +8,30 @@ function Create() {
     const [preparation, setPreparation] = useState('');
     const [image, setImage] = useState('');
     const [url, setUrl] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [ingredient, setIngredient] = useState('');
+    const IngredientInput = useRef(null);
+
+    const handleAddIngredient = (e) => {
+        const item = ingredient.trim();
+
+        if (item && !ingredients.includes(item)) {
+            setIngredients(prevItems => [...prevItems, item]);
+        }
+        setIngredient('');
+        IngredientInput.current.focus();
+        IngredientInput.current.value = '';
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (!title || !explanation || !preparation || !image || !url) {
-            alert('Please fill in all fields'); // Eksik alan kontrolü
+            alert('Please fill in all fields');
             return;
         }
 
-        const newRecipe = { title, explanation, preparation, image, url };
+        const newRecipe = { title, explanation, preparation, image, url, ingredients };
         console.log(newRecipe);
 
         setTitle('');
@@ -25,6 +39,8 @@ function Create() {
         setPreparation('');
         setImage('');
         setUrl('');
+        setIngredients([]);
+        setIngredient('');
     };
 
     return (
@@ -34,26 +50,40 @@ function Create() {
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" name='title' id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <input type="text" className="form-control" name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="explanation" className="form-label">Explanation</label>
-                        <textarea className="form-control" name='explanation' id="explanation" rows="3" value={explanation} onChange={(e) => setExplanation(e.target.value)}></textarea>
+                        <textarea className="form-control" name="explanation" id="explanation" rows="3" value={explanation} onChange={(e) => setExplanation(e.target.value)}></textarea>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="ingredients" className="form-label">
+                            Ingredients
+                            <ul>
+                                {ingredients.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                        </label>
+                        <div className="input-group">
+                            <input type="text" ref={IngredientInput} className="form-control" name="ingredients" id="ingredients" value={ingredient} onChange={(e) => setIngredient(e.target.value)} />
+                            <button className="btn btn-warning" type="button" id="button-addon2" onClick={handleAddIngredient}>Add</button>
+                        </div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="preparation" className="form-label">Preparation</label>
-                        <textarea className="form-control" name='preparation' id="preparation" rows="3" value={preparation} onChange={(e) => setPreparation(e.target.value)}></textarea>
+                        <textarea className="form-control" name="preparation" id="preparation" rows="3" value={preparation} onChange={(e) => setPreparation(e.target.value)}></textarea>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="image" className="form-label">Image</label>
-                        <input type="text" className="form-control" name='image' id="image" value={image} onChange={(e) => setImage(e.target.value)} />
+                        <input type="text" className="form-control" name="image" id="image" value={image} onChange={(e) => setImage(e.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="url" className="form-label">Url</label>
-                        <input type="text" className="form-control" name='url' id="url" value={url} onChange={(e) => setUrl(e.target.value)} />
+                        <input type="text" className="form-control" name="url" id="url" value={url} onChange={(e) => setUrl(e.target.value)} />
                     </div>
 
-                    <button type='submit' className='btn btn-warning'>
+                    <button type="submit" className="btn btn-warning">
                         Add Recipe
                     </button>
                 </form>
