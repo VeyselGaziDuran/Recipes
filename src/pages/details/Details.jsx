3002f25fa0d58b74f6bfaaabbca4f88af49ed1e3
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-24 19:01:53 +0300

    Error Handling Added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,19 +5,26 @@ import './Details.css';
 function Details() {
     const { id } = useParams();
     const [recipe, setRecipe] = useState(null);
-    const [isLoading, setIsLoading] = useState(true); // Eklenen kısım
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     const url = "http://localhost:3001/recipes/" + id;
 
     useEffect(() => {
         setIsLoading(true);
         fetch(url)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
             .then(res => {
                 setRecipe(res);
                 setIsLoading(false);
             })
             .catch(error => {
                 console.error('Error fetching recipe:', error);
+                setError('Error fetching recipe. Please try again.');
                 setIsLoading(false);
             });
     }, [url]);
@@ -25,6 +32,7 @@ function Details() {
     return (
         <div className="row mt-3">
             {isLoading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
 
             {
                 recipe && (
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,12 @@ import ProductCard from '../../components/ProductCard';
 import useFetch from '../../hooks/useFetch';
 
 function Home() {
-    const { recipe: recipes, isLoading } = useFetch('http://localhost:3001/recipes');
+    const { recipe: recipes, isLoading, error } = useFetch('http://localhost:3001/recipes');
 
     return (
         <div className="row mt-3">
-        {isLoading && <p>Loading...</p>}
+            {isLoading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
             {recipes && recipes.map(recipe => (
                 <ProductCard data={recipe} key={recipe.id} />
             ))}
