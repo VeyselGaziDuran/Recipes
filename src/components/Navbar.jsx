2879d71dd28f660ab6287a2e5b58011e2240646b
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-12-27 23:48:48 +0300

    Search Box
    Recipe Search Box Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { NavLink, Link } from "react-router-dom"
+import SearchBar from "./SearchBar"
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
             <div className="container">
                 <Link className="navbar-brand" to="/">Recipes</Link>
-                <ul className="navbar-nav">
+                <ul className="navbar-nav me-auto">
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/">Home</NavLink>
                     </li>
@@ -12,6 +13,7 @@ function Navbar() {
                         <NavLink className="nav-link" to="/create">Create</NavLink>
                     </li>
                 </ul>
+                <SearchBar />
             </div>
         </nav>
     )
